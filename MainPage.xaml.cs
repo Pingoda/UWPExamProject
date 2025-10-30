002bfb5a750114c697ae5d9815aff2b51daeb654
Pingoda <francescoclem915@gmail.com>
2025-10-30 10:32:06 +0100

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace UWPExamProject
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a <see cref="Frame">.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAnag_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnWebHttp_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLogManager_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+        }
+    }
+}
